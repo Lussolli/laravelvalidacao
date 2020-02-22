c235d45d92d11aa7d56c73c39daae9054f2cf1cd
Lussolli <leonardo.lussolli@gmail.com>
2020-02-22 11:52:14 -0300

    Formulário de cadastro de cliente implementado

--- resources/views/novocliente.blade.php
+++ resources/views/novocliente.blade.php
@@ -10,7 +10,45 @@
     <link rel="stylesheet" href="{{ asset('css/bootstrap/bootstrap.css') }}">
 </head>
 <body>
-    <h1>Olá, mundo!</h1>
+    <main role="main">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 offset-sm-2">
+                    <div class="card border mt-3">
+                        <div class="card-header">
+                            <div class="card-title">
+                                Cadastro de cliente
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <form action="/cliente" method="POST">
+                                <div class="form-group">
+                                    <label for="nome">Nome do cliente</label>
+                                    <input type="text" class="form-control" name="nome" id="nome" autofocus>
+                                </div>
+                                <div class="form-group">
+                                    <label for="idade">Idade</label>
+                                    <input type="number" class="form-control" name="idade" id="idade">
+                                </div>
+                                <div class="form-group">
+                                    <label for="endereco">Endereço do cliente</label>
+                                    <input type="text" class="form-control" name="endereco" id="endereco">
+                                </div>
+                                <div class="form-group">
+                                    <label for="email">E-mail do cliente</label>
+                                    <input type="text" class="form-control" name="email" id="email">
+                                </div>
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-primary btn-sm">Salvar</button>
+                                    <button type="cancel" class="btn btn-danger btn-sm">Cancelar</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
     
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
